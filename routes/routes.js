0e7bd2f88e92c919e35c8c1559aba6d2887d0f13
Balamuralikirushnan Santhujan <muralisanthu696@gmail.com>
2023-12-07 15:02:53 +0530

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,88 @@
+const express = require('express');
+
+const router = express.Router()
+
+module.exports = router;
+
+//Post Method
+// router.post('/post', (req, res) => {
+//     res.send('Post API')
+// })
+
+//Get all Method
+// router.get('/getAll', (req, res) => {
+//     res.send('Get All API')
+// })
+
+//Get by ID Method
+// router.get('/getOne/:id', (req, res) => {
+//     res.send('Get by ID API')
+// })
+
+//Update by ID Method
+// router.patch('/update/:id', (req, res) => {
+//     res.send('Update by ID API')
+// })
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
+
+const Model = require('../model/model');
+
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+
+
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get by ID Method
+router.get('/getOne/:id', async (req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Update by ID Method
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
